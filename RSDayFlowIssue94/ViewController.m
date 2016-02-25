9d8e7ff73ed3dfb031f3bd99cc42bf5159d6de39
Keith Yip <keithyip.kw@gmail.com>
2016-02-25 18:05:16 +0800

    Added example

--- RSDayFlowIssue94/ViewController.m
+++ RSDayFlowIssue94/ViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "ViewController.h"
+#import <RSDayFlow/RSDayFlow.h>
+
+@interface RSDFDatePickerView (Debug)
+
+@property (nonatomic, readonly, strong) RSDFDatePickerCollectionView *collectionView;
+
+@end
 
 @interface ViewController ()
+{
+    RSDFDatePickerView *mMonthView;
+    NSCalendar *mCalendar;
+}
 
 @end
 
@@ -16,7 +27,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    mCalendar = [NSCalendar currentCalendar];
+    
+    NSDateComponents *dc = [mCalendar components:NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[NSDate date]];
+    dc.day = 1;
+    dc.month += 6;
+    NSDate *selectedDate = [mCalendar dateFromComponents:dc];
+    
+    mMonthView = [[RSDFDatePickerView alloc] initWithFrame:self.view.bounds calendar:mCalendar];
+    mMonthView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+//    mMonthView.pagingEnabled = YES;
+//    mMonthView.delegate = self;
+//    mMonthView.dataSource = self;
+//    [mMonthView scrollToDate:mDate animated:NO];
+    [mMonthView selectDate:selectedDate];
+    [self.view addSubview:mMonthView];
+    
+    NSIndexPath *indexPath = [mMonthView.collectionView indexPathsForSelectedItems].firstObject;
+    if (indexPath.section >= mMonthView.collectionView.numberOfSections) {
+//        [mMonthView.collectionView layoutAttributesForItemAtIndexPath:indexPath];
+        abort();
+    }
 }
 
 - (void)didReceiveMemoryWarning {
